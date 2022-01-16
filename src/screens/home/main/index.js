28be60e8c270b7b5a3167a10106b027249dc4856
Eko Setiyo Budi Purnomo <LTL002742@lautan-luas.com>
2022-01-17 01:09:51 +0700

    eko : config force potrait

--- src/screens/home/main/index.js
+++ src/screens/home/main/index.js
@@ -4,6 +4,10 @@ import { useNavigation } from '@react-navigation/native';
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './style';
 import { HeaderComponent } from '@component';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { FlatGrid } from 'react-native-super-grid';
+
 
 export default function MainHomeScreen() {
   const navigation = useNavigation();
@@ -11,12 +15,39 @@ export default function MainHomeScreen() {
   useEffect(() => { }, []);
   const onPress = () => navigation.navigate('Base', { screen: 'Regulation' });
   return (
-    <View style={styles.container}>
+    <>
       <HeaderComponent />
-      <Text> Home AA </Text>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text>Press Here</Text>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.container}>
+        <Card style={{ margin: 20 }}>
+          <Card.Content>
+            <FlatGrid
+              itemDimension={75}
+              data={[1, 2, 3, 4, 5, 6]}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  style={{
+                    borderWidth: 1,
+                    borderColor: 'rgba(0,0,0,0.2)',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 75,
+                    height: 75,
+                    backgroundColor: '#fff',
+                    borderRadius: 8,
+                  }}
+                >
+                  <MaterialCommunityIcons name="home" color={'#01a699'} size={26} />
+                </TouchableOpacity>
+              )}
+            />
+          </Card.Content>
+        </Card>
+        <Text> Home AA </Text>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text>Press Here</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+
   );
 }
